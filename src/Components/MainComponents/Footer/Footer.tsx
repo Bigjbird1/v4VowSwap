d7ea7ed026216246c343faf1adbb75fe3da0cc06
Rasmus <76906581+razz1000@users.noreply.github.com>
2025-01-16 09:27:22 +0000

    Update Next to 14.2.23 , Clerk to 6.9.11 and Sendgrid to 8.1.

--- src/Components/MainComponents/Footer/Footer.tsx
+++ src/Components/MainComponents/Footer/Footer.tsx
@@ -146,7 +146,7 @@ export default function Footer() {
         </div>
         <div className="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24">
           <p className="text-xs leading-5 text-gray-500">
-            &copy; 2024 Example Company. All rights reserved. | Example VAT ID:
+            &copy; 2025 Example Company. All rights reserved. | Example VAT ID:
             12345678 | Address: Example Street, 12345 City, Country
           </p>
         </div>
